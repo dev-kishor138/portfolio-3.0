60dc76c521d649841752c6147713d7248bd24fe6
Kishor Mahmud <dev.kishor138@gmail.com>
2023-07-20 14:34:25 +0600

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+
+const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <div className="textBx">
+                <h2>Hello, I`m<br /><span>Muhammad Irshad.</span></h2>
+                <h3>I`m a Front End Developer</h3>
+                <a href="#" className="btn">About Me</a>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return (
+        <div>
+            <header>
+                <a href="/" className="logo">PortFolio</a>
+                <div className="toggle" onClick="toggleMenu();"></div>
+                <ul className="menu">
+                    <li><a href="#home" onClick="toggleMenu();">Home</a></li>
+                    <li><a href="#about" onClick="toggleMenu();">About</a></li>
+                    <li><a href="#services" onClick="toggleMenu();">Services</a></li>
+                    <li><a href="#work" onClick="toggleMenu();">Work</a></li>
+                    <li><a href="#testimonial" onClick="toggleMenu();">Testimonial</a></li>
+                    <li><a href="#contact" onClick="toggleMenu();">Contact</a></li>
+                </ul>
+            </header>
+        </div>
+    );
+};
+
+export default Navbar;
